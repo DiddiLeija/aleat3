402cc91a3ded9448718f2261d1062d761fd7ad14
Diego Ramirez <83621221+DiddiLeija@users.noreply.github.com>
2021-05-06 14:04:22 -0500

    Update setup.py
    Upgrade the "version" argument to 0.2.7dev1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aleat3",
-    version="0.2.6",
+    version="0.2.7dev1",
     author="Diego Ramirez",
     author_email="dr01191115@gmail.com",
     description="An aleatory syntaxes package. Third generation.",
