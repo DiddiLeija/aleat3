d6cf7f70e7c878ed9561556a560058102c3a65d9
Diego Ramirez <dr01191115@gmail.com>
2021-05-13 09:03:02 -0500

    Add a docstring for the applications folder
    Add a docstring that tells what does the "Transfer" class is doing there.

--- aleat3/applications/__init__.py
+++ aleat3/applications/__init__.py
@@ -1,3 +1,11 @@
+"""
+Use this as a patch if you can't run the
+examples correctly:
+
+    from aleat3.applications import apps
+    apps.exampleName.run()
+"""
+
 __all__ = ["apps"]
 
 from aleat3.applications import (diceInterface,
