1fe77def063a80923f79b3ad63220622503e593f
Diego Ramirez <dr01191115@gmail.com>
2021-05-24 09:10:17 -0500

    Change the large docstring with a link to GitHub
    Modify the __init__.__doc__, and replace it with a short link to GitHub, just like a "learn more" link...

--- aleat3/__init__.py
+++ aleat3/__init__.py
@@ -28,63 +28,8 @@ This object was created to give the folowing values:
    2. A 'coin' algorythm.
    3. A 'roulette' algorythm.
 
-In this edition we give a little guide of the posible custom methods of Aleatoryous. We also provided more objects.
-===============================================================================================================================
-USER GUIDE
-
-To call this Object, type this:
-
->>>from aleat3 import Aleatoryous
-
-Now, the object creation has two parameters that you should give:
-
->>>object = Aleatoryous("aleatory.dice", None)
-                                 ^         ^
-1. The 'mode'.
-2. The objects (only for the 'roulette' syntax. In other cases, type None).
-
-The method single() gets only one iteration, and can return a Value (String or Integer (In the case of the 'Dice')):
-
->>>coin = Aleatoryous("aleatory.coin", None)
->>>print(coin.single())
-Head
->>>print(coin.single())
-Head
->>>print(coin.single())
-Tails
-
-The method first_5() does the same 5 times and returns a list:
-
->>>dice = Aleatoryous("aleatory.dice", None)
->>>print(dice.first_5())
-[6, 3, 5, 6, 1]
-
-The methods first_5_basic() and refresh() are called internally ONLY.
-The 'Roulette' syntax is more complex than the others. Look at this:
-
->>>roul = Aleatoryous("aleatory.roulette", ["Go to sleep",
-...                                         "Play videogames",
-...                                         "Play the guitar",
-...                                         "Make a phone call to Castol",
-...                                         "Nothing"])
-
-But the rest is not so different:
-
->>>print(roul.single())
-Make a phone call to Castol
->>>print(dice.first_5())
-["Make a phone call to Castol", "Play videogames", "Go to sleep", "Nothing", "Make a phone call to Castol"]
-
-The methods first_10(), first_50() and first_100() are variations of first_5(), so they work as the same:
-
->>>dice = Aleatoryous("aleatory.dice", None)
->>>tot = dice.first_100()
->>>print(tot[45])
-4
->>>print(tot[99])
-2
->>>print(tot[12:14])
-[5, 1]
+Go to aleat3/constructor.py to check the code behind aleat3. This __init__ is only used when you import any file on the
+package.
 ===============================================================================================================================
 CREDITS
 
@@ -97,5 +42,5 @@ if __name__ == '__main__':
         print("Testing", k)
         d.changemode(k)
         print(d.single())
-        print(k, "OK")
+        print(k, "is OK")
         time.sleep(1)
