be651b5c9aa173063c7dfb7b19e89a1648784cef
Diego Ramirez <dr01191115@gmail.com>
2021-06-15 12:40:44 -0500

    Annotate on `init_errors.py`
    We need "typing.Any" for this.

--- aleat3/output/init_errors.py
+++ aleat3/output/init_errors.py
@@ -1,10 +1,11 @@
 """Generic functions to return error messages."""
+from typing import Any
 
-def parameter_bug(arg):
+def parameter_bug(arg: Any) -> str:
     return f"__init__() Invalid Syntax (Unexpected 'extras' given: {arg} when a list was expected)"
 
-def KI_bug():
+def KI_bug() -> str:
     return f"Program Interrupted by User..."
 
-def modal_bug(arg):
+def modal_bug(arg: str) -> str:
     return f"__init__() Invalid Syntax (Unexpected 'mode' given: {arg} when a list was expected)"
